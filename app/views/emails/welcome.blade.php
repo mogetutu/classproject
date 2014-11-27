1a58ea246d538148d36b1ba115c8c8feefe87986
loisenjoki <loisenjoki6@gmail.com>
2014-11-27 16:39:35 +0300

    final commit

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,6 +1,9 @@
 <h2>Dear {{$username}}</h2>
 <p>Welcome to SupperShoppers</p>
 <p>Have all the supermaket information on your phone? Its supershoppers</p>
-<p>Save time and money + Easy budgeting...!</p>
-
+<p>Save time and money + Easy budgeting...!</p><br>
+</br>
+<p>Happy shopping...!</p>
+<p>Thanks and kind regards,</p>
+<p>Suppershoppers</p>
  
--- app/views/pages/advertisments.blade.php
+++ app/views/pages/advertisments.blade.php
@@ -3,7 +3,7 @@
 <div id="offer">
 
 	<div class="flip3D">
-	  <div class="back">Offer!!!<br>
+	  <div class="back"><br>
 	  <!-- Buy a bread at an affordable price and make <br>
 	  live a strong morning!!! -->
 	  </div>
@@ -12,72 +12,72 @@
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 2 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/bic.jpg" width="250" height="200px">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 3 - Back
+	  <div class="back">
 	  </div>
 	  <div class="front">
 	  	<img src="assets/images/nakoff.jpg" width="280" height="200">
 	  </div>
 	 </div>
 	<div class="flip3D">
-	  <div class="back">Box 2 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/lenovo.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 3 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/sugar.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 2 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/free.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 3 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/sun.jpg" width="280" height="200">
 	  </div>
-	  <div class="back">Box 1 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/pamper.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 2 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/brand.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 3 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/flower.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 1 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/sugar.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 2 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/free.jpg" width="280" height="200">
 	  </div>
 	</div>
 	<div class="flip3D">
-	  <div class="back">Box 3 - Back</div>
+	  <div class="back"></div>
 	  <div class="front">
 	  	<img src="assets/images/foma.jpg" width="280" height="200">
 	  </div>
